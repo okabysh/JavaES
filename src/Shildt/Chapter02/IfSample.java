fd675c43aac303c3dd6adcc6b673bca95560c5c3
Oleg Kabysh <okabysh@gmail.com>
2017-03-20 21:45:21 +0200

    mod.1

---
+++ src/Shildt/Chapter02/IfSample.java
@@ -0,0 +1,10 @@
+package Shildt.Chapter02;
+
+/**
+ * Created by Oleg Kabysh on 20.03.2017.
+ */
+public class IfSample {
+    public static void main(String[] args) {
+        System.out.println("First");
+    }
+}
